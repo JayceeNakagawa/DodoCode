90026f6e48c622e91555f179d5bc2ffbc864b9c0
Jaycee Nakagawa <jayceem.nakagawa@gmail.com>
2023-02-24 17:33:47 -0800

    better ohm code and syntax + checking

--- test/Dodo.js
+++ test/Dodo.js
@@ -1,9 +1,9 @@
-import assert from "assert";
-import { add } from "../src/Dodo.js";
+// import assert from "assert";
+// import { add } from "../src/Dodo.js";
 
-describe("The compiler", () => {
-  it("gives the correct values for the add function", () => {
-    assert.equal(add(5, 8), 13);
-    assert.equal(add(5, 8), 3);
-  });
-});
+// describe("The compiler", () => {
+//   it("gives the correct values for the add function", () => {
+//     assert.equal(add(5, 8), 13);
+//     assert.equal(add(5, 8), 3);
+//   });
+// });
--- test/grammar.test.js
+++ test/grammar.test.js
@@ -2,34 +2,37 @@ import assert from "assert/strict";
 import fs from "fs";
 import ohm from "ohm-js";
 
-//yea...make sure these pass. Just go through all of them.
 const syntaxChecks = [
-  // ["all numeric literal forms", "print(8 * 89.123);"],
-  // ["complex expressions", "print(83 * ((((-((((13 / 21)))))))) + 1 - 0);"],
-  // ["all unary operators", "print (-3); print (!false);"],
-  // ["all binary operators", "print x && y || z * 1 / 2 ** 3 + 4 < 5;"],
-  // ["all arithmetic operators", "let x = (!3) * 2 + 4 - (-7.3) * 8 ** 13 / 1;"],
-  // ["all relational operators", "let x = 1<(2<=(3==(4!=(5 >= (6>7)))));"],
-  // ["all logical operators", "let x = true && false || (!false);"],
-  // ["the conditional operator", "print x ? y : z;"],
-  // ["end of program inside comment", "print(0); // yay"],
-  // ["comments with no text are ok", "print(1);//\nprint(0);//"],
-  // ["non-Latin letters in identifiers", "コンパイラ = 100;"],
-  ["all numeric literal forms", "tweet:1+2+3;"],
-  ["binary operators", "int x = 3*0+1-2;"],
+  ["string literal", 'tweet:"Hello, World!";'],
+  ["variable declaration", "int x = 5;"],
+  ["array declarations", "tweet:[1,2];"],
+  ["function with two parameters", "action f(int x, int y) {}"],
+  ["if,elseif,else statement", "pet (x<=1) {} feed (x<=2) {} interact {}"],
+  ["while statement", "sleep(x==10){}"],
+  ["boolean literal declaration", "boolean x = true; boolean y = false;"],
+  ["character variables", "cat garfield = r;"],
+  ["strings variables", 'cat garfield = "lasagna";'],
+  ["int variables", "food chocolate = 2;"],
+  ["arithmetic operators", "int x = 3+3-3*3**2/2%1- -1;"],
+  ["comments", "int x = 3; //hi there "],
+  ["shift operators", "int x = 3 << 5 >> 8 << 13 >> 21;"],
+  ["relational operators", "tweet:1<2||1<=2||1==2||1!=2||1>=2||1>2;"],
+  ["special numbers", "int x  = 3.1-E;"],
+  ["or/and chaining", "tweet: 1&&2&&3&&4; tweet: 1||2||3||4;"],
+  ["unary op", "boolean x = true; x = !x;"],
 ];
 
 const syntaxErrors = [
-  // ["non-letter in an identifier", "ab😭c = 2", /Line 1, col 3/],
-  // ["malformed number", "x= 2.", /Line 1, col 6/],
-  // ["missing semicolon", "x = 3 y = 1", /Line 1, col 7/],
-  // ["a missing right operand", "print(5 -", /Line 1, col 10/],
-  // ["a non-operator", "print(7 * ((2 _ 3)", /Line 1, col 15/],
-  // ["an expression starting with a )", "x = );", /Line 1, col 5/],
-  // ["a statement starting with expression", "x * 5;", /Line 1, col 3/],
-  // ["an illegal statement on line 2", "print(5);\nx * 5;", /Line 2, col 3/],
-  // ["a statement starting with a )", "print(5);\n) * 5", /Line 2, col 1/],
-  // ["an expression starting with a *", "x = * 71;", /Line 1, col 5/],
+  ["non-letter in an identifier", "int ab😭c = 2", /Line 1, col 7/],
+  ["missing semicolon", "boolean x = true", /Line 1, col 17/],
+  ["malformed number", "x= 2.", /Line 1, col 6/],
+  ["a missing right operand", "tweet:5 -", /Line 1, col 10/],
+  ["a non-operator", "x=7 * 2 _ 3;", /Line 1, col 9/],
+  ["an expression starting with a )", "x=);", /Line 1, col 3/],
+  ["a statement starting with expression", "x * 5;", /Line 1, col 3/],
+  ["an illegal statement on line 2", "tweet:5;\nx * 5;", /Line 2, col 3/],
+  ["a statement starting with a )", ") * 5", /Line 1, col 1/],
+  ["an expression starting with a *", "x = * 71;", /Line 1, col 5/],
 ];
 
 describe("The grammar", () => {
