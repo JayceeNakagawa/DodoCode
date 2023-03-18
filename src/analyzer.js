2a042d4b2233af8328bd5dca61a0d34be3cf43a0
Jaycee Nakagawa <jayceem.nakagawa@gmail.com>
2023-03-17 19:19:29 -0700

    removing some comments

--- src/analyzer.js
+++ src/analyzer.js
@@ -10,36 +10,39 @@ function error(message, node) {
 
 const DodoGrammar = ohm.grammar(fs.readFileSync("src/Dodo.ohm"));
 
+function check(condition, message, node) {
+  if (!condition) error(message, node);
+}
+
+class Context {
+  constructor(parent = null) {
+    this.parent = parent;
+    this.locals = new Map();
+  }
+  add(name, entity, node) {
+    check(!this.locals.has(name), `${name} has already been declared`, node);
+    this.locals.set(name, entity);
+    return entity;
+  }
+  get(name, expectedType, node) {
+    let entity;
+    for (let context = this; context; context = context.parent) {
+      entity = context.locals.get(name);
+      if (entity) break;
+    }
+    check(entity, `${name} has not been declared`, node);
+    check(
+      entity.constructor === expectedType,
+      `${name} was expected to be a ${expectedType.name}`,
+      node
+    );
+    return entity;
+  }
+}
+
 export default function analyze(sourceCode) {
-  //   const analyzer = DodoGrammar.createSemantics().addOperation("rep", {
-  //     Program(body) {
-  //       return new core.Program(body.children.map((s) => s.rep())); //because it has a star? if you have _iter, you can use:
-  //       //_iter(...children) in bella code.
-  //     },
-  //     //may need to use new keyword here and there...
-  //     PrintStmt(_tweet, _left, argument, _right, _semicolon) {
-  //       return core.PrintStatement(argument.rep());
-  //     },
-  //     //building things for...things...uhhh... analyzer func for ... ohm
-  //     //i don't have some of thesee...i need to use my brain bruh.
-  //     //coo keyword, var object, _eq not gonna use, bird not gonna use, initalizer.
-  //     //do that for all...???
-  //     //some return different things...
-  //     VarDec(_coo, variable, _eq, initalizer, _bird) {
-  //       return new core.VariableDeclaration(variable.rep(), initalizer.rep());
-  //     },
-  //     AssignmentStmnt() {},
-  //     IfStmt() {},
-  //     id() {},
-  //     Var() {},
-  //     Exp_add() {},
-  //     Exp_sub() {},
-  //     Term_parens() {},
-  //     numeral() {},
-  //     strlit() {},
-  //   });
+  let context = new Context();
   const match = DodoGrammar.match(sourceCode);
   if (!match.succeeded()) error(match.message);
   console.log("Yay, program is syntactically correct");
-  // return analyzer(match).rep();
 }
